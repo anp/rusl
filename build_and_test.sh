74d6a62b8b9c45ce568cd5c3ea9029e0c4ef86ac
Adam Perry <adam.n.perry@gmail.com>
2016-04-10 15:43:58 -0700

    Updating test suite to libc-test, further work on the porting build system.

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,5 +1,71 @@
 #!/usr/bin/env bash
 
+set -e
 
+BASE_DIR=`pwd`
+BUILD_DIR=${BASE_DIR}/build
+MUSL_SRC_DIR=${BASE_DIR}/musl
+TESTS_SRC_DIR=${BASE_DIR}/libc-test
+NUM_CPUS=`grep -c ^processor /proc/cpuinfo`
 
-echo "hello world"
+clean_and_exit_build_dir() {
+    cd ${BASE_DIR}
+    rm -dr ${BUILD_DIR}
+}
+
+build_musl() {
+    cd ${MUSL_SRC_DIR}
+    ./configure --prefix=${BUILD_DIR}/usr --disable-shared --enable-static
+    make -j ${NUM_CPUS}
+    make install
+    cd ${BASE_DIR}
+}
+
+build_mursl() {
+    cd ${BASE_DIR}
+    cargo build --release
+    cp target/release/libmursl.a ${BUILD_DIR}/usr/lib
+}
+
+combine_mursl_and_musl() {
+    # move the two archives into a tempdir
+    cd ${BUILD_DIR}/usr/lib
+    LIB_TEMP_DIR=${BUILD_DIR}/usr/lib/temp
+    mkdir ${LIB_TEMP_DIR}
+    mv libmursl.a ${LIB_TEMP_DIR}/
+
+    # extract the object files
+    # and recombine them so that the Rust symbols are alongside the C symbols
+    cd ${LIB_TEMP_DIR}
+    ar -x libmursl.a
+    ar -r ../libc.a ${LIB_TEMP_DIR}/*.o
+    rm -dr ${LIB_TEMP_DIR}
+}
+
+build_and_run_tests() {
+    cd ${TESTS_SRC_DIR}
+
+    cp config.mak.def config.mak
+    echo "CFLAGS += --static -isystem ${BUILD_DIR}/usr/include -B${BUILD_DIR}/usr/lib -L${BUILD_DIR}/usr/lib" >> config.mak
+    echo "LDFLAGS += --static -isystem ${BUILD_DIR}/usr/include -B${BUILD_DIR}/usr/lib -L${BUILD_DIR}/usr/lib" >> config.mak
+    make CC=${BUILD_DIR}/usr/bin/musl-gcc
+    cat ${TESTS_SRC_DIR}/src/REPORT | grep FAIL > ${BASE_DIR}/mursl_failures
+
+    echo "#####################################################################"
+    echo "#####################################################################"
+    echo "#####################################################################"
+    echo "#####################################################################"
+    echo "Tests that failed on mursl but not on vanilla musl:"
+    echo "#####################################################################"
+    grep -v -F -x -f ${BASE_DIR}/baseline_failures ${BASE_DIR}/mursl_failures
+
+    cd ${BASE_DIR}
+}
+
+#trap clean_and_exit_build_dir EXIT
+
+mkdir -p ${BUILD_DIR}/usr
+build_musl
+build_mursl
+combine_mursl_and_musl
+build_and_run_tests
---
+++ clean_all.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+BASE_DIR=`pwd`
+BUILD_DIR=${BASE_DIR}/build
+MUSL_SRC_DIR=${BASE_DIR}/musl
+TESTS_SRC_DIR=${BASE_DIR}/libc-test
+
+rm -dr ${BUILD_DIR}
+cd ${MUSL_SRC_DIR} && make clean
+cd ${TESTS_SRC_DIR} && make cleanall
+cd ${BASE_DIR} && cargo clean
