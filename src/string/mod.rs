a6c77d592d9ca07578d74b4202922a87f2cd8c4d
Adam Perry <adam.n.perry@gmail.com>
2016-04-11 01:43:14 -0700

    Porting musl/src/string/strlen.c

--- src/string/mod.rs
+++ src/string/mod.rs
@@ -1,2 +1,3 @@
-pub mod strcpy;
 pub mod stpcpy;
+pub mod strcpy;
+pub mod strlen;
---
+++ src/string/strlen.rs
@@ -0,0 +1,12 @@
+use core::usize;
+
+use c_types::*;
+
+#[no_mangle]
+pub unsafe extern "C" fn strlen(s: *const c_schar) -> size_t {
+    // TODO(adam) convert to checking word-size chunks
+    for i in 0.. {
+        if *s.offset(i) == 0 { return i as usize; }
+    }
+    return usize::MAX;
+}
