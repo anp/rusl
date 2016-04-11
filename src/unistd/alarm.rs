47b64039f144f6017ea7b2b28598cb5b48193127
Adam Perry <adam.n.perry@gmail.com>
2016-04-10 22:17:57 -0700

    Should have double checked git status before last commit.

---
+++ src/unistd/alarm.rs
@@ -0,0 +1,21 @@
+use c_types::*;
+
+use time::{itimerval, timespec, timeval, ITIMER_REAL};
+
+#[no_mangle]
+pub unsafe extern "C" fn alarm(seconds: c_uint) -> c_uint {
+    let mut it = itimerval {
+        it_interval: timeval {
+            tv_sec: 0,
+            tv_usec: 0,
+        },
+    	it_value: timeval {
+            tv_sec: seconds as time_t,
+            tv_usec: 0,
+        }
+    };
+
+    syscall!(SETITIMER, ITIMER_REAL, &mut it as *mut itimerval, &mut it as *mut itimerval);
+
+    (it.it_value.tv_sec + !!it.it_value.tv_usec) as c_uint
+}
