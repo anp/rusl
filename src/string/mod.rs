9ee4f8697956a63848c7714559fffda9a645518c
Adam Perry <adam.n.perry@gmail.com>
2016-04-11 01:51:18 -0700

    Porting musl/src/string/strcmp.c

--- src/string/mod.rs
+++ src/string/mod.rs
@@ -1,3 +1,4 @@
 pub mod stpcpy;
 pub mod strcpy;
 pub mod strlen;
+pub mod strcmp;
---
+++ src/string/strcmp.rs
@@ -0,0 +1,16 @@
+use core::i32;
+
+use c_types::*;
+
+#[no_mangle]
+pub unsafe extern "C" fn strcmp(l: *const c_schar, r: *const c_schar) -> c_int {
+    // TODO(adam) convert to checking word-size chunks
+    for i in 0.. {
+        let lc = *l.offset(i);
+        let rc = *r.offset(i);
+        if lc == 0 || lc != rc {
+            return (lc - rc) as c_int;
+        }
+    }
+    return i32::MAX;
+}
