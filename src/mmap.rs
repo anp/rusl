6933679fd779c698e1f3f6970f18a0f361cc7d26
Adam Perry <adam.n.perry@gmail.com>
2016-05-03 15:34:25 -0700

    Porting mlock().

--- src/mmap.rs
+++ src/mmap.rs
@@ -11,3 +11,7 @@ pub unsafe extern fn __madvise(address: *mut c_void, len: usize, advice: c_int) 
 pub unsafe extern fn mincore(address: *mut c_void, len: usize, vec: *mut u8) -> c_int {
     syscall!(MINCORE, address, len, vec) as c_int
 }
+
+pub unsafe extern fn mlock(address: *const c_void, len: usize) -> c_int {
+    syscall!(MLOCK, address, len) as c_int
+}
