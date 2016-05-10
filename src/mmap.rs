669a25025bf9be577951dc4e4947194e02630fea
Adam Perry <adam.n.perry@gmail.com>
2016-05-10 09:45:33 -0700

    Porting munmap().

--- src/mmap.rs
+++ src/mmap.rs
@@ -6,6 +6,12 @@ use platform::mman::*;
 use thread::vmlock::__vm_wait;
 
 #[no_mangle]
+pub unsafe extern "C" fn __munmap(start: *mut c_void, len: size_t) -> c_int {
+    __vm_wait();
+    syscall!(MUNMAP, start, len) as c_int
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn __mmap(start: *mut c_void,
                                 len: size_t,
                                 prot: c_int,
@@ -32,28 +38,6 @@ pub unsafe extern "C" fn __mmap(start: *mut c_void,
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn mmap(start: *mut c_void,
-                              len: size_t,
-                              prot: c_int,
-                              flags: c_int,
-                              fd: c_int,
-                              off: off_t)
-                              -> *mut c_void {
-    __mmap(start, len, prot, flags, fd, off)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn mmap64(start: *mut c_void,
-                                len: size_t,
-                                prot: c_int,
-                                flags: c_int,
-                                fd: c_int,
-                                off: off_t)
-                                -> *mut c_void {
-    __mmap(start, len, prot, flags, fd, off)
-}
-
-#[no_mangle]
 pub unsafe extern "C" fn madvise(address: *mut c_void, len: usize, advice: c_int) -> c_int {
     syscall!(MADVISE, address, len, advice) as c_int
 }
@@ -75,3 +59,30 @@ pub unsafe extern "C" fn mlock(address: *const c_void, len: usize) -> c_int {
 
 #[no_mangle]
 pub unsafe extern "C" fn mlockall(flags: c_int) -> c_int { syscall!(MLOCKALL, flags) as c_int }
+
+
+// aliases
+#[no_mangle]
+pub unsafe extern "C" fn munmap(start: *mut c_void, len: size_t) -> c_int { __munmap(start, len) }
+
+#[no_mangle]
+pub unsafe extern "C" fn mmap(start: *mut c_void,
+                              len: size_t,
+                              prot: c_int,
+                              flags: c_int,
+                              fd: c_int,
+                              off: off_t)
+                              -> *mut c_void {
+    __mmap(start, len, prot, flags, fd, off)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn mmap64(start: *mut c_void,
+                                len: size_t,
+                                prot: c_int,
+                                flags: c_int,
+                                fd: c_int,
+                                off: off_t)
+                                -> *mut c_void {
+    __mmap(start, len, prot, flags, fd, off)
+}
