680cf139c00318cea9e3df68f989723aa7c26df1
Adam Perry <adam.n.perry@gmail.com>
2016-05-11 19:01:42 -0700

    Porting expand_heap.c.
    Accepting malloc_brk_fail.c failures as SIGSEGV instead of status 1.

--- src/malloc/expand_heap.rs
+++ src/malloc/expand_heap.rs
@@ -1,6 +1,11 @@
+use core::num::Wrapping;
+use core::usize;
+
 use c_types::*;
 use environ::AUXV_PTR;
-
+use errno::{set_errno, ENOMEM};
+use platform::mman::*;
+use mmap::__mmap;
 
 /// Comment from original musl C function:
 ///
@@ -37,4 +42,61 @@ pub extern "C" fn traverses_stack_p(old: usize, new: usize) -> c_int {
     }
 
     0
+}
+
+// Expand the heap in-place if brk can be used, or otherwise via mmap,
+// using an exponential lower bound on growth by mmap to make
+// fragmentation asymptotically irrelevant. The size argument is both
+// an input and an output, since the caller needs to know the size
+// allocated, which will be larger than requested due to page alignment
+// and mmap minimum size rules. The caller is responsible for locking
+// to prevent concurrent calls.
+
+#[no_mangle]
+pub unsafe extern "C" fn __expand_heap(pn: *mut size_t) -> *mut c_void {
+    static mut brk: usize = 0;
+    static mut mmap_step: c_uint = 0;
+
+    let mut n = *pn;
+
+    if n > ((usize::MAX / 2) - PAGE_SIZE as usize) {
+        set_errno(ENOMEM);
+        return 0 as *mut c_void;
+    }
+
+    n += (-Wrapping(n)).0 & (PAGE_SIZE as usize - 1);
+
+    if brk == 0 {
+        brk = syscall!(BRK, 0);
+        brk += (-Wrapping(brk)).0 & (PAGE_SIZE - 1) as usize;
+    }
+
+    if n < (usize::MAX - brk) && traverses_stack_p(brk, brk + n) == 0 &&
+       syscall!(BRK, brk + n) == brk + n {
+        *pn = n;
+        brk += n;
+        return (brk - n) as *mut c_void;
+    }
+
+    let min = (PAGE_SIZE << (mmap_step / 2)) as usize;
+
+    if n < min {
+        n = min;
+    }
+
+    let area = __mmap(0 as *mut c_void,
+                      n,
+                      PROT_READ | PROT_READ,
+                      MAP_PRIVATE | MAP_ANONYMOUS,
+                      -1,
+                      0);
+
+    if area == MAP_FAILED {
+        return 0 as *mut c_void;
+    }
+
+    *pn = n;
+    mmap_step += 1;
+
+    area
 }
--- src/platform/linux-x86_64/mman.rs
+++ src/platform/linux-x86_64/mman.rs
@@ -4,5 +4,64 @@ pub const PAGE_SIZE: off_t = 4096;
 
 pub const MAP_FAILED: *mut c_void = 0xffffffffffffffff as *mut c_void;
 
+pub const PROT_NONE: c_int = 0;
+pub const PROT_READ: c_int = 1;
+pub const PROT_WRITE: c_int = 2;
+pub const PROT_EXEC: c_int = 4;
+pub const PROT_GROWSDOWN: c_int = 0x01000000;
+pub const PROT_GROWSUP: c_int = 0x02000000;
+
+pub const MAP_SHARED: c_int = 0x01;
+pub const MAP_PRIVATE: c_int = 0x02;
 pub const MAP_FIXED: c_int = 0x10;
+
+pub const MAP_TYPE: c_int = 0x0f;
+pub const MAP_FILE: c_int = 0x00;
+pub const MAP_ANON: c_int = 0x20;
+pub const MAP_ANONYMOUS: c_int = MAP_ANON;
+pub const MAP_32BIT: c_int = 0x40;
+pub const MAP_NORESERVE: c_int = 0x4000;
+pub const MAP_GROWSDOWN: c_int = 0x0100;
+pub const MAP_DENYWRITE: c_int = 0x0800;
+pub const MAP_EXECUTABLE: c_int = 0x1000;
+pub const MAP_LOCKED: c_int = 0x2000;
+pub const MAP_POPULATE: c_int = 0x8000;
+pub const MAP_NONBLOCK: c_int = 0x10000;
+pub const MAP_STACK: c_int = 0x20000;
+pub const MAP_HUGETLB: c_int = 0x40000;
+
+pub const POSIX_MADV_NORMAL: c_int = 0;
+pub const POSIX_MADV_RANDOM: c_int = 1;
+pub const POSIX_MADV_SEQUENTIAL: c_int = 2;
+pub const POSIX_MADV_WILLNEED: c_int = 3;
+pub const POSIX_MADV_DONTNEED: c_int = 0;
+
+pub const MS_ASYNC: c_int = 1;
+pub const MS_INVALIDATE: c_int = 2;
+pub const MS_SYNC: c_int = 4;
+
+pub const MCL_CURRENT: c_int = 1;
+pub const MCL_FUTURE: c_int = 2;
+pub const MCL_ONFAULT: c_int = 4;
+
+pub const MADV_NORMAL: c_int = 0;
+pub const MADV_RANDOM: c_int = 1;
+pub const MADV_SEQUENTIAL: c_int = 2;
+pub const MADV_WILLNEED: c_int = 3;
+pub const MADV_DONTNEED: c_int = 4;
+pub const MADV_REMOVE: c_int = 9;
+pub const MADV_DONTFORK: c_int = 10;
+pub const MADV_DOFORK: c_int = 11;
+pub const MADV_MERGEABLE: c_int = 12;
+pub const MADV_UNMERGEABLE: c_int = 13;
+pub const MADV_HUGEPAGE: c_int = 14;
+pub const MADV_NOHUGEPAGE: c_int = 15;
+pub const MADV_DONTDUMP: c_int = 16;
+pub const MADV_DODUMP: c_int = 17;
+pub const MADV_HWPOISON: c_int = 100;
+pub const MADV_SOFT_OFFLINE: c_int = 101;
+
+pub const MREMAP_MAYMOVE: c_int = 1;
 pub const MREMAP_FIXED: c_int = 2;
+
+pub const MLOCK_ONFAULT: c_int = 0x01;
