cadb205218551bb2992f6d57cdeb31da94af5a08
A.J. Gardner <aaron.j.gardner@gmail.com>
2017-01-12 19:09:27 -0600

    Mimic musl's strlen

--- src/string/mod.rs
+++ src/string/mod.rs
@@ -3,3 +3,11 @@ pub mod strcpy;
 pub mod strlen;
 pub mod strcmp;
 pub mod strspn;
+
+const ONES: usize  = 0x0101010101010101_usize;
+const HIGHS: usize = 0x8080808080808080_usize;
+
+#[inline(always)]
+fn has_zero(x: usize) -> bool {
+    (x.wrapping_sub(ONES) & !x & HIGHS) != 0
+}
--- src/string/strlen.rs
+++ src/string/strlen.rs
@@ -1,14 +1,26 @@
-use core::usize;
+use core::{ usize, mem };
+use super::has_zero;
 
-use c_types::*;
+use c_types::{ c_schar, size_t };
 
 #[no_mangle]
 pub unsafe extern "C" fn strlen(s: *const c_schar) -> size_t {
-    // TODO(adam) convert to checking word-size chunks
-    for i in 0.. {
-        if *s.offset(i) == 0 {
-            return i as usize;
+    let mut t = s;
+    while t as usize % mem::size_of::<size_t>() != 0 {
+        if *t == 0 {
+            return t as size_t - s as size_t
         }
+        t = t.offset(1)
     }
-    usize::MAX
+
+    let mut w = t as *const size_t;
+    while !has_zero(*w) {
+        w = w.offset(1)
+    }
+
+    t = w as *const c_schar;
+    while *t != 0 {
+        t = t.offset(1)
+    }
+    t as size_t - s as size_t
 }
