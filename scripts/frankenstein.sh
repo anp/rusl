4b9ed02ea50470d2055b0c10a41946d4bedd588d
A.J. Gardner <aaron.j.gardner@gmail.com>
2018-01-13 16:56:00 -0600

    Use separate directory for test_rusl test runs

--- scripts/frankenstein.sh
+++ scripts/frankenstein.sh
@@ -8,17 +8,25 @@ BUILD_DIR=${BASE_DIR}/bld
 
 # move the two archives into a tempdir
 cd "${BUILD_DIR}"/usr/lib
-LIB_TEMP_DIR="${BUILD_DIR}"/usr/lib/temp
-mkdir "${LIB_TEMP_DIR}"
-cp "${BASE_DIR}"/target/release/librusl.a "${LIB_TEMP_DIR}"/
+LIB_RUSL_DIR="${BUILD_DIR}"/usr/librusl
+mkdir "${LIB_RUSL_DIR}"
+cp "${BASE_DIR}"/target/release/librusl.a "${LIB_RUSL_DIR}"/
+cp -t "${LIB_RUSL_DIR}"/ *.a *.specs # get .o files in a bit
 
-cd "${LIB_TEMP_DIR}"
-# delete all ported object files from libc.a
+cd "${LIB_RUSL_DIR}"
+
 PORTED_OBJECTS=()
 while IFS= read -r line; do
   PORTED_OBJECTS+=(${line})
 done < "${BASE_DIR}"/ported_objects
-ar -dv ../libc.a "${PORTED_OBJECTS[@]}"
+
+# delete all ported object files from libc.a
+ar -dv libc.a "${PORTED_OBJECTS[@]}"
+
+# add in all .o files from librusl.a
 ar -x librusl.a
-ar -r ../libc.a "${LIB_TEMP_DIR}"/*.o
-rm -r "${LIB_TEMP_DIR}"
+ar -r libc.a *.o
+
+rm *.o
+# get remaining .o files from lib
+cp -t ./ ../lib/*.o
--- scripts/test_rusl.sh
+++ scripts/test_rusl.sh
@@ -11,7 +11,7 @@ NUM_CPUS=$(grep -c ^processor /proc/cpuinfo)
 cd "${TESTS_SRC_DIR}"
 
 cp config.mak.def config.mak
-echo "CFLAGS += -static -isystem ${BUILD_DIR}/usr/include -B${BUILD_DIR}/usr/lib -L${BUILD_DIR}/usr/lib" >> config.mak
-echo "LDFLAGS += -static -isystem ${BUILD_DIR}/usr/include -B${BUILD_DIR}/usr/lib -L${BUILD_DIR}/usr/lib" >> config.mak
+echo "CFLAGS += -static -isystem ${BUILD_DIR}/usr/include -B${BUILD_DIR}/usr/librusl -L${BUILD_DIR}/usr/librusl" >> config.mak
+echo "LDFLAGS += -static -isystem ${BUILD_DIR}/usr/include -B${BUILD_DIR}/usr/librusl -L${BUILD_DIR}/usr/librusl" >> config.mak
 make -j "${NUM_CPUS}" CC="${BUILD_DIR}"/usr/bin/musl-gcc
 grep FAIL < "${TESTS_SRC_DIR}"/src/REPORT > "${BASE_DIR}"/rusl_failures
